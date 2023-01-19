76b671ae2c0010dbd01cc70388850999282739e4
kongchatsupasri <36078290+kongchatsupasri@users.noreply.github.com>
2023-01-19 22:46:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ def read_park_description_json():
 
 # st.cache(persist = True)
 def how_to_img():
-  d = {'how1': './photo-1585409677983-0f6c41ca9c3b.jpeg',
+  d = {'how1': './tab-01.png',
       'how2': './tab-01.png',
       'how3': './tab-02.png',
       'how4': './tab-03.png'}
